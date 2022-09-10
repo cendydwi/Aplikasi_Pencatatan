871e121eed67505c797dd789959ab31a0032f1db
cendydwie47 <cendydwie47@gmail.com>
2022-09-10 16:15:45 +0700

    Final

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -65,7 +65,7 @@
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
             <a href="/" class="app-brand-link">
-              <span class="app-brand-text demo menu-text fw-bolder ms-2">pencatatan</span>
+              <span class="app-brand-text demo menu-text fw-bolder ms-2" style="text-transform: uppercase;">pencatatan</span>
             </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
